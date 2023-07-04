1f7802e85ca30378d304d3ccbcd9b32152789e43
홍승희 <seung0476@gmail.com>
2023-07-04 14:37:28 +0900

    자동차 관리 DTO - 진만

--- JDBC_Project/src/DTO/Car_Superintend.java
+++ JDBC_Project/src/DTO/Car_Superintend.java
@@ -11,12 +11,11 @@ import lombok.ToString;
 @Setter
 public class Car_Superintend {
 	//자동차 관리 DTO - 진만
-	private int car_id;
+	private String car_no;
 	private String car_garde;
 	private String carType;
 	private String rent_Type;
 	private int price;
 	private int insurance;
-	private String car_no;
 	private String PL;
 }
