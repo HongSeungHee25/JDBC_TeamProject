1ff57d4c2eb7f5933fe280ac27eef80ac9b1d1b3
홍승희 <seung0476@gmail.com>
2023-07-04 14:37:47 +0900

    자동차 렌트 예약 DTO - 종화

--- JDBC_Project/src/DTO/Car_rent.java
+++ JDBC_Project/src/DTO/Car_rent.java
@@ -14,8 +14,8 @@ import lombok.ToString;
 public class Car_rent {
 	//자동차 렌트예약 DTO - 종화
 	private int rent_no;
-	private String customer_id;
-	private int car_id;
+	private String name;
+	private String car_no;
 	private Date rent_start;
 	private Date rent_end;
 
