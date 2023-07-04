f2124835f1d34081fc00c751b9b4e92f6db658a3
홍승희 <seung0476@gmail.com>
2023-07-04 09:19:41 +0900

    ERD 테이블_기본키, 외래키, 컬럼명 수정 후 생성 - 승희

--- JDBC_Project/src/ERD - 테이블 생성(PK,FK).sql
+++ JDBC_Project/src/ERD - 테이블 생성(PK,FK).sql
@@ -15,26 +15,25 @@ DROP TABLE manager CASCADE CONSTRAINTS;
 -- 자동차 관리 테이블
 CREATE TABLE car
 (
-	car_id number NOT NULL,
+	car_no varchar2(30) NOT NULL,
 	car_grade varchar2(10) NOT NULL,
 	car_type varchar2(20) NOT NULL,
 	rent_Type varchar2(10),
 	price number NOT NULL,
 	insurance number NOT NULL,
-	car_no varchar2(30) NOT NULL UNIQUE,
 	PL varchar2(10) NOT NULL,
-	PRIMARY KEY (car_id)
+	PRIMARY KEY (car_no)
 );
 
 -- 자동차 검사 테이블
 CREATE TABLE car_inspection
 (
-	car_id number NOT NULL,
+	car_no varchar2(30) NOT NULL,
 	Inspection_type varchar2(30) NOT NULL,
 	Last_date date NOT NULL,
 	Next_date date NOT NULL,
-	PRIMARY KEY (car_id),
-	FOREIGN KEY (car_id) REFERENCES car (car_id)
+	PRIMARY KEY (car_no),
+	FOREIGN KEY (car_no) REFERENCES car (car_no)
 );
 
 -- 자동차 렌트 예약 테이블
@@ -42,12 +41,12 @@ CREATE TABLE car_rent
 (
 	rent_no number NOT NULL,
 	customer_id varchar2(20) NOT NULL,
-	car_id number NOT NULL,
+	car_no varchar2(30) NOT NULL,
 	rent_start date NOT NULL,
 	rent_end date NOT NULL,
 	PRIMARY KEY (rent_no),
 	FOREIGN KEY (customer_id) REFERENCES customer (customer_id),
-	FOREIGN KEY (car_id) REFERENCES car (car_id)
+	FOREIGN KEY (car_no) REFERENCES car (car_no)
 );
 
 -- 회원 관리 테이블
@@ -77,9 +76,9 @@ CREATE TABLE Payment
 	payment_day  date NOT NULL,
 	money number,
 	payment_method varchar2(30) NOT NULL,
-	car_id number NOT NULL,
+	car_no varchar2(30) NOT NULL,
 	PRIMARY KEY (payment_id),
 	FOREIGN KEY (customer_id) REFERENCES customer (customer_id),
 	FOREIGN KEY (rent_no) REFERENCES car_rent (rent_no),
-	FOREIGN KEY (car_id) REFERENCES car (car_id)
+	FOREIGN KEY (car_no) REFERENCES car (car_no)
 );
