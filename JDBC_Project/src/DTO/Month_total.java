fc18e2b96b7c9cfdcac40ed05f45cfd2042c42b3
홍승희 <seung0476@gmail.com>
2023-07-04 14:38:38 +0900

    월별 토탈 DTO - 승희

---
+++ JDBC_Project/src/DTO/Month_total.java
@@ -0,0 +1,17 @@
+package DTO;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.ToString;
+
+@Getter
+@ToString
+@AllArgsConstructor
+@Builder
+public class Month_total {
+	//월별 토탈 DTO - 승희
+	private String months;
+	private int total;
+	
+}
