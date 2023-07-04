317f91fe8909b32cb22d6a994f273c75e4551281
홍승희 <seung0476@gmail.com>
2023-07-04 15:51:06 +0900

    자동차 검사 테이블 데이터 - 종화

--- JDBC_Project/src/자동차 검사 테이블 데이터.sql
+++ JDBC_Project/src/자동차 검사 테이블 데이터.sql
@@ -1,51 +1,20 @@
 -- 자동차 검사 데이터
-insert into car_inspection
-values ('101하 1001', ' 엔진오일, 브레이크 오일, 브레이크 패드, 타이어', 
-'2023-03-20', '2024-04-12');
-insert into car_inspection
-values ('101하 1002', ' 엔진오일, 브레이크 오일, 브레이크 패드, 타이어', 
-'2023-03-20', '2024-04-12');
-insert into car_inspection
-values ('101허 1001', ' 엔진오일, 브레이크 오일, 브레이크 패드, 타이어', 
-'2023-03-20', '2024-04-12');
-insert into car_inspection
-values ('101허 1002', ' 엔진오일, 브레이크 오일, 브레이크 패드, 타이어', 
-'2023-03-20', '2024-04-12');
-insert into car_inspection
-values ('120호 2111', ' 엔진오일, 브레이크 오일, 브레이크 패드, 타이어', 
-'2022-08-05', '2023-09-09');
-insert into car_inspection
-values ('120호 2112', ' 엔진오일, 브레이크 오일, 브레이크 패드, 타이어', 
-'2022-08-05', '2023-09-09');
-insert into car_inspection
-values ('142하 3431', ' 엔진오일, 브레이크 오일, 브레이크 패드, 타이어', 
-'2022-08-05', '2023-09-09');
-insert into car_inspection
-values ('142하 3432', ' 엔진오일, 브레이크 오일, 브레이크 패드, 타이어', 
-'2022-08-05', '2023-09-09');
-insert into car_inspection
-values ('142허 4311', ' 엔진오일, 브레이크 오일, 브레이크 패드, 타이어', 
-'2023-06-20', '2024-04-24');
-insert into car_inspection
-values ('142허 4312', ' 엔진오일, 브레이크 오일, 브레이크 패드, 타이어', 
-'2023-06-20', '2024-04-24');
-values ('124호 5713', ' 엔진오일, 브레이크 오일, 브레이크 패드, 타이어', 
-'2023-06-20', '2024-04-24');
-insert into car_inspection
-values ('124호 5714', ' 엔진오일, 브레이크 오일, 브레이크 패드, 타이어', 
-'2023-06-20', '2024-04-24');
-insert into car_inspection
-values ('114하 9280', ' 엔진오일, 브레이크 오일, 브레이크 패드, 타이어', 
-'2022-11-20', '2023-10-29');
-insert into car_inspection
-values ('114하 9281', ' 엔진오일, 브레이크 오일, 브레이크 패드, 타이어', 
-'2022-11-20', '2023-10-29');
-values ('150하 6773', ' 엔진오일, 브레이크 오일, 브레이크 패드, 타이어', 
-'2022-11-20', '2023-10-29');
-insert into car_inspection
-values ('150하 6774', ' 엔진오일, 브레이크 오일, 브레이크 패드, 타이어', 
-'2022-11-20', '2023-10-29');
-
+insert into car_inspection values ('101하 1001', '엔진오일', '2023-03-20', '2024-04-12');
+insert into car_inspection values ('101하 1002', '브레이크 오일', '2023-03-20', '2024-04-12');
+insert into car_inspection values ('101허 1011', '엔진오일', '2023-03-20', '2024-04-12');
+insert into car_inspection values ('101허 1012', '브레이크 패드', '2023-03-20', '2024-04-12');
+insert into car_inspection values ('120호 2111', '타이어', '2022-08-05', '2023-09-09');
+insert into car_inspection values ('120호 2112', '엔진오일', '2022-08-05', '2023-09-09');
+insert into car_inspection values ('142하 3431', '브레이크 오일', '2022-08-05', '2023-09-09');
+insert into car_inspection values ('142하 3432', ' 엔진오일', '2022-08-05', '2023-09-09');
+insert into car_inspection values ('142허 4311', '브레이크 오일', '2023-06-20', '2024-04-24');
+insert into car_inspection values ('142허 4312', '브레이크 오일', '2023-06-20', '2024-04-24');
+insert into car_inspection values ('124호 5713', '브레이크 패드', '2023-06-20', '2024-04-24');
+insert into car_inspection values ('124호 5714', '엔진오일', '2023-06-20', '2024-04-24');
+insert into car_inspection values ('114하 9280', '브레이크 오일', '2022-11-20', '2023-10-29');
+insert into car_inspection values ('114하 9281', '타이어', '2022-11-20', '2023-10-29');
+insert into car_inspection values ('150하 6773', '브레이크 오일', '2022-11-20', '2023-10-29');
+insert into car_inspection values ('150하 6774', '브레이크 패드', '2022-11-20', '2023-10-29');
 
 
 
