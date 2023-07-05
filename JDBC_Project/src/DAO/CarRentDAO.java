842b6c6e707e6bc14a77aa5e9bf8384d59f37ef6
홍승희 <seung0476@gmail.com>
2023-07-05 20:25:15 +0900

    자동차 렌트 DAO - 병인

--- JDBC_Project/src/DAO/CarRentDAO.java
+++ JDBC_Project/src/DAO/CarRentDAO.java
@@ -14,6 +14,12 @@ import DTO.Car_rent;
 
 public class CarRentDAO {
 	
+	private static CarRentDAO rentDAO = new CarRentDAO();
+	private CarRentDAO() {}
+	public static CarRentDAO getCarRentDAO() {
+		return rentDAO;
+	}
+	
 	//자동차 예약 조회 DAO - 병인
 	public List<Car_rent> selectAll() throws SQLException{
 	      Connection conn = OracleUtility.getConnection();
@@ -22,8 +28,12 @@ public class CarRentDAO {
 	      ResultSet rs = ps.executeQuery();
 	      List<Car_rent> list = new ArrayList<>();
 	      while(rs.next()) {
-	    	  list.add(new Car_rent(rs.getInt(1), rs.getString(2), rs.getString(3), rs.getDate(4), rs.getDate(5)));
+	    	  list.add(new Car_rent(rs.getInt(1), rs.getString(2), rs.getString(3), rs.getString(4), rs.getString(5)));
 	      }
+	      conn.close();
+		   ps.close();
+		   rs.close();
+	      
 	      return list;
 	}
 	
@@ -37,9 +47,32 @@ public class CarRentDAO {
 		   Car_rent cr = null;
 		   if(rs.next()) {
 		      
-		      cr = new Car_rent(rs.getInt(1), rs.getString(2), rs.getString(3), rs.getDate(4), rs.getDate(5));
+		      cr = new Car_rent(rs.getInt(1), rs.getString(2), rs.getString(3), rs.getString(4), rs.getString(5));
 		   }
+		   conn.close();
+		   ps.close();
+		   rs.close();
+		   
 		   return cr;
 		}
+	public int insertReserve(Car_rent data) throws SQLException{
+		Connection conn = OracleUtility.getConnection();
+		String select = "insert into car_rent "
+				+ "values(?,?,?,?,?)";
+		PreparedStatement ps = conn.prepareStatement(select);
+		Car_rent cr = null;
+		ps.setInt(1, cr.getRent_no());
+		ps.setString(2, cr.getName());
+		ps.setString(3, cr.getCar_no());
+		ps.setString(4, cr.getRent_start());
+		ps.setString(5, cr.getRent_end());
+		
+		
+		int result = ps.executeUpdate();
+		
+		conn.close();
+		ps.close();
+		return result;
+	}
 	
 }
