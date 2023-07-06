f1ad1bf0bf5a85e210a3743b06e0150a0ef39032
홍승희 <seung0476@gmail.com>
2023-07-06 13:29:32 +0900

    회원 관리 DTO - 지수

--- JDBC_Project/src/DTO/Customer.java
+++ JDBC_Project/src/DTO/Customer.java
@@ -3,17 +3,19 @@ package DTO;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.Setter;
 import lombok.ToString;
 
+@Setter
 @ToString
 @Getter
 @AllArgsConstructor
 @Builder
 public class Customer {
 	//회원 관리 DTO - 지수
-	private String pw;
 	private String name;
 	private String customer_id;
+	private String pw;
 	private String phone;
 	private String licence;
 }
