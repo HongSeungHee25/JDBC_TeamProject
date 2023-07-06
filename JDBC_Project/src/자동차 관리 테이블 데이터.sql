2662bdca604a8547b276c510028e93be96102ffd
홍승희 <seung0476@gmail.com>
2023-07-06 13:46:50 +0900

    자동차 관리 테이블 데이터 - 진만

--- JDBC_Project/src/자동차 관리 테이블 데이터.sql
+++ JDBC_Project/src/자동차 관리 테이블 데이터.sql
@@ -11,7 +11,5 @@ INSERT INTO CAR c VALUES ('142허 4311', '중형차', '소나타', NULL, 53240, 
 INSERT INTO CAR c VALUES ('142허 4312', '중형차', 'K5', NULL, 49400, 24280, '휘발유');
 INSERT INTO CAR c VALUES ('124호 5713', '중형차', '스포티지', NULL, 65000, 24280, '경유');
 INSERT INTO CAR c VALUES ('124호 5714', '중형차', '투싼', NULL, 67000, 28650, '경유');
-INSERT INTO CAR c VALUES ('114하 9280', '대형차', '펠리세이드', NULL, 91200, 48680, '경유');
-INSERT INTO CAR c VALUES ('114하 9281', '대형차', '모하비', NULL, 88760, 47050, '경유');
 INSERT INTO CAR c VALUES ('150하 6773', '대형차', '카니발', NULL, 85050, 25570, '휘발유');
 INSERT INTO CAR c VALUES ('150하 6774', '대형차', '스타리아', NULL, 81460, 24760, '경유');
