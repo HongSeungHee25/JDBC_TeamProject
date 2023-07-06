6607c0d66669f574bb6c434a9843cffadcc14dd1
홍승희 <seung0476@gmail.com>
2023-07-06 13:31:15 +0900

    자동차 예약 DAO - 병인

--- JDBC_Project/src/DAO/CarRentDAO.java
+++ JDBC_Project/src/DAO/CarRentDAO.java
@@ -21,40 +21,32 @@ public class CarRentDAO {
 	}
 	
 	//자동차 예약 조회 DAO - 병인
-	public List<Car_rent> selectAll() throws SQLException{
-	      Connection conn = OracleUtility.getConnection();
-	      String select = "select * from car_rent";
-	      PreparedStatement ps = conn.prepareStatement(select);
-	      ResultSet rs = ps.executeQuery();
-	      List<Car_rent> list = new ArrayList<>();
-	      while(rs.next()) {
-	    	  list.add(new Car_rent(rs.getInt(1), rs.getString(2), rs.getString(3), rs.getString(4), rs.getString(5)));
-	      }
-	      conn.close();
-		   ps.close();
-		   rs.close();
-	      
-	      return list;
-	}
-	
-	//자동차 렌트 예약 번호로 조회 DAO - 병인
-	public Car_rent selectByRentNo(int rent_no) throws SQLException{
-		   Connection conn = OracleUtility.getConnection();
-		   String select = "select * from car_rent where rent_no = ?";
-		   PreparedStatement ps = conn.prepareStatement(select);
-		   ps.setInt(1, rent_no );
-		   ResultSet rs = ps.executeQuery();
-		   Car_rent cr = null;
-		   if(rs.next()) {
-		      
-		      cr = new Car_rent(rs.getInt(1), rs.getString(2), rs.getString(3), rs.getString(4), rs.getString(5));
-		   }
-		   conn.close();
-		   ps.close();
-		   rs.close();
-		   
-		   return cr;
+		public List<Car_rent> selectAll() throws SQLException{
+		      Connection conn = OracleUtility.getConnection();
+		      String select = "select * from car_rent";
+		      PreparedStatement ps = conn.prepareStatement(select);
+		      ResultSet rs = ps.executeQuery();
+		      List<Car_rent> list = new ArrayList<>();
+		      while(rs.next()) {
+		    	  list.add(new Car_rent(rs.getInt(1), rs.getString(2), rs.getString(3), rs.getDate(4), rs.getDate(5)));
+		      }
+		      return list;
 		}
+	
+		//자동차 렌트 예약 번호로 조회 DAO - 병인
+		public Car_rent selectByRentNo(int rent_no) throws SQLException{
+			   Connection conn = OracleUtility.getConnection();
+			   String select = "select * from car_rent where rent_no = ?";
+			   PreparedStatement ps = conn.prepareStatement(select);
+			   ps.setInt(1, rent_no );
+			   ResultSet rs = ps.executeQuery();
+			   Car_rent cr = null;
+			   if(rs.next()) {
+			      
+			      cr = new Car_rent(rs.getInt(1), rs.getString(2), rs.getString(3), rs.getDate(4), rs.getDate(5));
+			   }
+			   return cr;
+			}
 	public int insertReserve(Car_rent data) throws SQLException{
 		Connection conn = OracleUtility.getConnection();
 		String select = "insert into car_rent "
@@ -64,8 +56,8 @@ public class CarRentDAO {
 		ps.setInt(1, cr.getRent_no());
 		ps.setString(2, cr.getName());
 		ps.setString(3, cr.getCar_no());
-		ps.setString(4, cr.getRent_start());
-		ps.setString(5, cr.getRent_end());
+		ps.setDate(4, cr.getRent_start());
+		ps.setDate(5, cr.getRent_end());
 		
 		
 		int result = ps.executeUpdate();
