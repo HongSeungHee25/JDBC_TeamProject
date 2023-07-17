02eed3b59d79e810dc46a7970b413fc32d86c049
홍승희 <seung0476@gmail.com>
2023-07-17 10:11:36 +0900

    로그인 화면 구현 - 승희, 로그인 JDBC 연결 - 병인

--- JDBC_Project/src/Car_rent_Program/Login.java
+++ JDBC_Project/src/Car_rent_Program/Login.java
@@ -1,13 +1,14 @@
 package Car_rent_Program;
 
+import java.awt.Color;
 import java.awt.EventQueue;
 import java.awt.Font;
 import java.awt.Graphics;
+import java.awt.Image;
 import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.sql.SQLException;
-import java.text.SimpleDateFormat;
 
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
@@ -22,45 +23,46 @@ import javax.swing.SwingUtilities;
 
 import DAO.CustomerDAO;
 import DTO.Customer;
-import gui.Choicecar_1;
 
 //로그인 화면 구현 - 승희
 //로그인 JDBC 연결 - 병인
 @SuppressWarnings("serial")
 public class Login extends JFrame {
-	
-	private JButton btnMypage;
-	Customer customer; 
-	private JScrollPane jScrollPane;
-    private ImageIcon icon;
+
+    private JButton btnMypage;
+    Customer customer;
+    private JScrollPane jScrollPane;
     private JLabel label;
     private JLabel label_1;
     private JButton loginButton;
     private JButton joinButton;
     private JButton Managerbutton;
+    ImageIcon gifIcon;
     
-	String loggedInUserName;
+    String loggedInUserName;
 
     public Login() {
-    	setTitle("렌트가 예약 프로그램");
-    	setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    	setIconImage(Toolkit.getDefaultToolkit().getImage("C:\\Users\\82108\\OneDrive\\바탕 화면\\자동차 로고\\로그인Test.jpg"));
+        setTitle("SOULCAR");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setIconImage(Toolkit.getDefaultToolkit().getImage("./image/차량선택 백그라운드.jpg")); // 프로젝트 내부 경로로 변경
         setSize(900, 800);
-        setLocationRelativeTo(null);  
-        
-        icon = new ImageIcon("./image/로그인백그라운드.jpg");
+        setLocationRelativeTo(null);
+
+        gifIcon = new ImageIcon("./image/소울카로그인GIF.gif"); // GIF 이미지 파일 경로로 변경
 
         JPanel back = new JPanel() {
             public void paintComponent(Graphics g) {
-                g.drawImage(icon.getImage(), 0, 0, null);
-                setOpaque(false);
                 super.paintComponent(g);
+                Image image = gifIcon.getImage();
+                g.drawImage(image, 0, 0, getWidth(), getHeight(), this);
             }
         };
-        back.setLayout(null);
 
+        back.setLayout(null);
+        setContentPane(back);
 
         label = new JLabel("ID : ");
+        label.setForeground(new Color(255, 255, 255));
         label.setBounds(280, 450, 100, 30);
         label.setFont(new Font("휴먼둥근헤드라인", Font.PLAIN, 16));
         back.add(label);
@@ -70,6 +72,7 @@ public class Login extends JFrame {
         back.add(textField);
 
         label_1 = new JLabel("PW : ");
+        label_1.setForeground(new Color(255, 255, 255));
         label_1.setBounds(280, 500, 100, 30);
         label_1.setFont(new Font("휴먼둥근헤드라인", Font.PLAIN, 16));
         back.add(label_1);
@@ -97,24 +100,23 @@ public class Login extends JFrame {
         btnMypage.setFont(new Font("휴먼둥근헤드라인", Font.PLAIN, 12));
         btnMypage.setBounds(504, 550, 100, 30);
         back.add(btnMypage);
-        
+
         // 마이페이지 버튼 액션 리스너
         btnMypage.addActionListener(new ActionListener() {
-			
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				
-				String id = textField.getText();
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+                String id = textField.getText();
                 String password = new String(passwordField.getPassword());
                 try {
                     customer = CustomerDAO.getCustomerDAO().login(id, password);
                     if (customer != null) {
-                    	// 로그인 성공 시 실행할 코드 작성
-                    	DataHolder.loggedInUserName = customer.getName();
-                    	JOptionPane.showMessageDialog(null, "마이페이지로 이동! "+ customer.getName()+" 님 환영합니다.\n확인 버튼을 누르면 마이페이지로 이동합니다.");
+                        // 로그인 성공 시 실행할 코드 작성
+                        DataHolder.loggedInUserName = customer.getName();
+                        JOptionPane.showMessageDialog(null, "마이페이지로 이동! " + customer.getName() + " 님 환영합니다.\n확인 버튼을 누르면 마이페이지로 이동합니다.");
 
-                    	openMyPage(customer.getName());
-
+                        openMyPage(customer.getName());
 
                         // 현재 로그인 창 닫기
                         dispose();
@@ -125,27 +127,26 @@ public class Login extends JFrame {
                 } catch (SQLException ex) {
                     ex.printStackTrace();
                 }
-				
-			}
-		});
-        
+
+            }
+        });
+
         // 로그인 버튼에 액션 리스너 추가
         loginButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-            	
+
                 String id = textField.getText();
                 String password = new String(passwordField.getPassword());
                 try {
                     customer = CustomerDAO.getCustomerDAO().login(id, password);
                     if (customer != null) {
-                    	// 로그인 성공 시 실행할 코드 작성
-                    	DataHolder.loggedInUserName = customer.getName();
-                    	JOptionPane.showMessageDialog(null, "로그인 성공! "+ customer.getName()+" 님 환영합니다.\n확인 버튼을 누르면 차량 등급을 선택합니다.");
+                        // 로그인 성공 시 실행할 코드 작성
+                        DataHolder.loggedInUserName = customer.getName();
+                        JOptionPane.showMessageDialog(null, "로그인 성공! " + customer.getName() + " 님 환영합니다.\n확인 버튼을 누르면 차량 등급을 선택합니다.");
 
-                    	// 차량 등급 패널로 이동
+                        // 차량 등급 패널로 이동
                         openChoiceCarType(customer.getName()); // Pass the loggedInUserName
-
 
                         // 현재 로그인 창 닫기
                         dispose();
@@ -158,10 +159,6 @@ public class Login extends JFrame {
                 }
             }
         });
-
-
-       
-
 
         // 회원 가입 버튼에 액션 리스너 추가
         joinButton.addActionListener(new ActionListener() {
@@ -172,8 +169,8 @@ public class Login extends JFrame {
                 join.setVisible(true);
             }
         });
-    
-     // 관리자로 로그인 버튼에 액션 리스너 추가
+
+        // 관리자로 로그인 버튼에 액션 리스너 추가
         Managerbutton.addActionListener(e -> {
             JPasswordField M_passwordField = new JPasswordField();
             int option = JOptionPane.showOptionDialog(this, M_passwordField, "관리자 비밀번호",
@@ -188,33 +185,28 @@ public class Login extends JFrame {
                 // Arrays.fill(passwordChars, '0');
             }
         });
-    
-    
-    
 
         jScrollPane = new JScrollPane(back);
         setContentPane(jScrollPane);
-        
-    }
-    
- // 차량 선택 화면으로 이동
- protected void openChoiceCarType(String name) throws SQLException {
 
-	 Choicecar choicecar =  new Choicecar(name); // 여기서 생성자 한 번 호출 
-     choicecar.setVisible(true);
-     //choicecar.main(null);		// 여기서 choicecar 클래스 내부 메인 한 번 호출.
-		
-	}
- 
- 	private void openMyPage(String name) {
- 		MyPage mypage = new MyPage(name);
- 		mypage.setVisible(true);
- 	}
- 
+    }
+
+    // 차량 선택 화면으로 이동
+    protected void openChoiceCarType(String name) throws SQLException {
+        Choicecar choicecar = new Choicecar(name); // 여기서 생성자 한 번 호출
+        choicecar.setVisible(true);
+        //        choicecar.main(null);		// 여기서 choicecar 클래스 내부 메인 한 번 호출.
+    }
+
+    private void openMyPage(String name) {
+        MyPage mypage = new MyPage(name);
+        mypage.setVisible(true);
+    }
+
     private void openManagerScreen() {
         SwingUtilities.invokeLater(() -> {
             Manager manager = null;
-			manager = new Manager();
+            manager = new Manager();
             JFrame frame = new JFrame("관리자");
             frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
             frame.setSize(900, 800);
@@ -223,7 +215,7 @@ public class Login extends JFrame {
             frame.setVisible(true);
         });
     }
-    
+
     public static void main(String[] args) {
         EventQueue.invokeLater(new Runnable() {
             public void run() {
@@ -237,9 +229,10 @@ public class Login extends JFrame {
         });
     }
 
-public class DataHolder {
-    public static String loggedInUserName;  // 전역 변수로 사용자 이름 저장
+    public class DataHolder {
+        public static String loggedInUserName; // 전역 변수로 사용자 이름 저장
 
-    // ... 추가 필요한 데이터 저장
+        // ... 추가 필요한 데이터 저장
+    }
 }
-}
+
