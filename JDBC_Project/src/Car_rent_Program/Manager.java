40f8d55434db68b79a5fedd9d252a628fcb1efe8
홍승희 <seung0476@gmail.com>
2023-07-19 13:38:04 +0900

    Update Manager.java

--- JDBC_Project/src/Car_rent_Program/Manager.java
+++ JDBC_Project/src/Car_rent_Program/Manager.java
@@ -35,7 +35,6 @@ import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTabbedPane;
 import javax.swing.JTable;
-import javax.swing.JTextArea;
 import javax.swing.JTextField;
 import javax.swing.ScrollPaneConstants;
 import javax.swing.SwingConstants;
