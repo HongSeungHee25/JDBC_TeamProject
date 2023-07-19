4d69934085b5ffef7267840e2df6c886bf5a1d34
홍승희 <seung0476@gmail.com>
2023-07-19 17:53:12 +0900

    주요 SQL - 팀원

--- JDBC_Project/src/주요 SQL.sql
+++ JDBC_Project/src/주요 SQL.sql
@@ -13,15 +13,23 @@ SELECT RENT_NO , name, CAR_NO ,TO_CHAR(RENT_START,'yyyy-mm-dd') AS rent_start,TO
 FROM CAR_RENT cr WHERE name = '홍승희' AND rownum = 1 ORDER BY RENT_NO DESC;
 
 -- 회원 로그인해서 마이페이지에서 예약 내역 조회하기
-SELECT car_no , car_type, to_char(rent_start,'yyyy-mm-dd') AS rent_start, TO_CHAR(rent_end, 'yyyy-mm-dd') AS rent_end,
-(PRICE + INSURANCE) AS MONEY 
-FROM CAR JOIN CAR_RENT 
-using(car_no)
-WHERE NAME = '홍승희'
-ORDER BY rent_start;
+SELECT
+    b.car_no,
+    a.car_type,
+    TO_CHAR(b.rent_start, 'yyyy-mm-dd') AS rent_start,
+    TO_CHAR(b.rent_end, 'yyyy-mm-dd') AS rent_end,
+    (a.PRICE + a.INSURANCE) * (b.rent_end - b.rent_start) AS MONEY
+FROM
+    CAR a
+JOIN
+    CAR_RENT b ON a.car_no = b.car_no
+WHERE
+    b.NAME = '홍승희'
+ORDER BY
+    b.rent_start;
 
 -- 관리자페이지에서 회원별 매출 등급 넣어서 확인
-SELECT name, total_money, payment_method,
+SELECT name, total_money, 
 CASE
 WHEN total_money >= 500000 THEN 'VIP'
 WHEN total_money >= 300000 THEN 'GOLD'
@@ -29,10 +37,10 @@ WHEN total_money >= 200000 THEN 'SILVER'
 ELSE 'FAMILY'
 END AS grade
 FROM (
-SELECT p.name, SUM(money) AS total_money, p.payment_method
+SELECT p.name, SUM(money) AS total_money
 FROM PAYMENT p
 JOIN CAR c ON p.car_no = c.car_no
-GROUP BY p.name, p.payment_method
+GROUP BY p.name
 ) subquery
 ORDER BY name;
 
@@ -47,14 +55,29 @@ FROM CAR c
 LEFT JOIN CAR_RENT cr ON c.CAR_NO = cr.CAR_NO;
 
 -- 월별 토탈 매출 조회
-SELECT 
-to_char(payment_day,'yyyy-mm') AS months, payment_method, money AS total
-FROM ( SELECT p.payment_id, p.name, p.payment_day, p.money, p.payment_method, p.car_no
-FROM PAYMENT p 
-JOIN CAR c
-ON p.car_no = c.car_no)
-GROUP BY to_char(payment_day,'yyyy-mm'), payment_method, money
-ORDER BY months DESC;
+SELECT
+    TO_CHAR(payment_day, 'yyyy-mm') AS months,
+    payment_method,
+    SUM(money) AS total
+FROM
+    (
+        SELECT
+            p.payment_id,
+            p.name,
+            p.payment_day,
+            p.money,
+            p.payment_method,
+            p.car_no
+        FROM
+            PAYMENT p
+        JOIN
+            CAR c ON p.car_no = c.car_no
+    )
+GROUP BY
+    TO_CHAR(payment_day, 'yyyy-mm'),
+    payment_method
+ORDER BY
+    months DESC;
 
 
 
