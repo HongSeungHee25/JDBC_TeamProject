bb79ca412cb279a5277dfe29f8621d2e79e0f113
홍승희 <seung0476@gmail.com>
2023-07-19 13:38:30 +0900

    주요 SQL 문 - 승희

--- JDBC_Project/src/주요 SQL.sql
+++ JDBC_Project/src/주요 SQL.sql
@@ -1,25 +1,68 @@
--- 예약 상태 이름 
+-- 중복 예약 불가능
+SELECT CASE 
+WHEN cr.RENT_START > SYSDATE THEN 0
+WHEN cr.RENT_START < SYSDATE AND cr.RENT_END > SYSDATE THEN 0
+ELSE 1 
+END AS IS_AVAILABLE 
+FROM CAR c 
+LEFT JOIN CAR_RENT cr ON c.CAR_NO = cr.CAR_NO 
+WHERE c.CAR_NO = '101하 1204';
+
+-- 최근 예약 정보 1개
+SELECT RENT_NO , name, CAR_NO ,TO_CHAR(RENT_START,'yyyy-mm-dd') AS rent_start,TO_CHAR(RENT_END,'yyyy-mm-dd') AS RENT_END  
+FROM CAR_RENT cr WHERE name = '홍승희' AND rownum = 1 ORDER BY RENT_NO DESC;
+
+-- 회원 로그인해서 마이페이지에서 예약 내역 조회하기
+SELECT 
+c.CAR_NO,
+c.CAR_TYPE,
+TO_CHAR(cr.RENT_START, 'yyyy-mm-dd') AS RENT_START,
+TO_CHAR(cr.RENT_END, 'yyyy-mm-dd') AS RENT_END,
+p.PAYMENT_METHOD, 
+c.PRICE + c.INSURANCE AS money 
+FROM 
+CAR c 
+JOIN CAR_RENT cr ON c.CAR_NO = cr.CAR_NO 
+JOIN PAYMENT p ON c.CAR_NO = p.CAR_NO 
+WHERE
+cr.NAME = '홍승희'
+ORDER BY RENT_START;
+
+-- 관리자페이지에서 회원별 매출 등급 넣어서 확인
+SELECT name, total_money, payment_method,
+CASE
+WHEN total_money >= 500000 THEN 'VIP'
+WHEN total_money >= 300000 THEN 'GOLD'
+WHEN total_money >= 200000 THEN 'SILVER'
+ELSE 'FAMILY'
+END AS grade
+FROM (
+SELECT p.name, SUM(money) AS total_money, p.payment_method
+FROM PAYMENT p
+JOIN CAR c ON p.car_no = c.car_no
+GROUP BY p.name, p.payment_method
+) subquery
+ORDER BY name;
+
+-- 차량 대여가능 여부 조회
 SELECT c.CAR_NO, c.CAR_GRADE, c.CAR_TYPE,
-  (CASE
-    WHEN cr.RENT_START > SYSDATE THEN '예약 중'
-    WHEN cr.RENT_START < SYSDATE AND cr.RENT_END > SYSDATE THEN '대여 중'
-    ELSE '대여 가능'
-  END) AS RENT_TYPE, c.PRICE , c.INSURANCE ,c.PL
+(CASE
+WHEN cr.RENT_START > SYSDATE THEN '예약 중'
+WHEN cr.RENT_START < SYSDATE AND cr.RENT_END > SYSDATE THEN '대여 중'
+ELSE '대여 가능'
+END) AS RENT_TYPE, c.PRICE , c.INSURANCE ,c.PL
 FROM CAR c
 LEFT JOIN CAR_RENT cr ON c.CAR_NO = cr.CAR_NO;
 
--- 총 토탈 가격
-SELECT p.payment_id, p.name, p.payment_day, c.PRICE + c.INSURANCE AS money, p.PAYMENT_METHOD ,p.car_no
-FROM PAYMENT p JOIN CAR c 
-ON p.car_no = c.CAR_NO 
-ORDER BY p.PAYMENT_ID ;
+-- 월별 토탈 매출 조회
+SELECT 
+to_char(payment_day,'yyyy-mm') AS months, payment_method, money AS total
+FROM ( SELECT p.payment_id, p.name, p.payment_day, p.money, p.payment_method, p.car_no
+FROM PAYMENT p 
+JOIN CAR c
+ON p.car_no = c.car_no)
+GROUP BY to_char(payment_day,'yyyy-mm'), payment_method, money
+ORDER BY months DESC;
 
--- 월 별 매출 조회
-SELECT to_char(payment_day,'yyyy-mm') AS months, PAYMENT_METHOD, sum(money) AS total
-FROM (SELECT p.payment_id, p.name, p.payment_day, c.PRICE + c.INSURANCE AS money, p.PAYMENT_METHOD ,p.car_no
-		FROM PAYMENT p JOIN CAR c 
-		ON p.car_no = c.CAR_NO)
-GROUP BY to_char(payment_day,'yyyy-mm'),PAYMENT_METHOD
-ORDER BY months;
 
 
